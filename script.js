f47418af3f52d850f563b30070b221006e63ab73
HigorRochaGamex <157840691+minecraftbedrockpro@users.noreply.github.com>
2024-12-04 13:09:05 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -11,3 +11,18 @@ function showSection(sectionId) {
 
 // Exibir a seção de PC por padrão ao carregar a página
 showSection('downloads-pc');
+
+
+
+for (let i = 0; i < 100; i++) {
+    const star = document.createElement('div');
+    star.classList.add('star');
+    const size = Math.random() * 5 + 2; // Tamanho aleatório entre 2 e 7px
+    star.style.width = `${size}px`;
+    star.style.height = `${size}px`;
+    star.style.top = `${Math.random() * 100}vh`;
+    star.style.left = `${Math.random() * 100}vw`;
+    star.style.animationDuration = `${Math.random() * 2 + 2}s`; // Duração aleatória entre 2 e 4 segundos
+    star.style.animationDelay = `${Math.random() * 2}s`;
+    document.body.appendChild(star);
+}
