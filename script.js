787844c613c8c3eafb2b3dac9b5b07986a758101
Higor <157840691+minecraftbedrockpro@users.noreply.github.com>
2025-03-25 16:03:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function showDownloadButton(link, option, version, icon) {
     // Se a opção for Android, mostrar dois botões em vez de um único
     if (option === 'Android') {
         downloadButton.innerHTML = `
-            <button class="button" onclick="window.location.href='Instale o 32 bit por enquanto.'">Baixar 64bit</button>
+            <button class="button" onclick="window.location.href='https://anonymfile.com/f/558d7e44-8e0c-4889-b17e-93d91f3fae1c'">Baixar 64bit</button>
             <button class="button" onclick="window.location.href='https://anonymfile.com/f/558d7e44-8e0c-4889-b17e-93d91f3fae1c'">Baixar 32bit</button>
         `;
     } else {
@@ -31,19 +31,4 @@ function showDownloadButton(link, option, version, icon) {
             <div class="version">${version}</div>
         </div>
     `;
-}
-// Função para adicionar as estrelas dinamicamente
-for (let i = 0; i < 100; i++) {
-    const star = document.createElement('div'); // Cria o elemento da estrela
-    star.classList.add('star'); // Adiciona a classe para estilizar
-
-    const size = Math.random() * 5 + 2; // Tamanho aleatório entre 2 e 7px
-    star.style.width = `${size}px`;
-    star.style.height = `${size}px`;
-    star.style.top = `${Math.random() * 100}vh`; // Posicionamento aleatório vertical
-    star.style.left = `${Math.random() * 100}vw`; // Posicionamento aleatório horizontal
-    star.style.animationDuration = `${Math.random() * 2 + 2}s`; // Duração da animação aleatória
-    star.style.animationDelay = `${Math.random() * 2}s`; // Atraso aleatório na animação
-
-    document.body.appendChild(star); // Adiciona a estrela ao corpo da página
 }
